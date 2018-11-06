a26227a3f84bec2e6b5997bcb84b25b37f759079
Carl Hörberg <carl.hoerberg@gmail.com>
2018-11-06 16:57:45 +0100

    crystal 0.27 compability

--- src/amqp/protocol.cr
+++ src/amqp/protocol.cr
@@ -85,7 +85,7 @@ module AMQP::Protocol
                    @reply_to = "",
                    @expiration = "",
                    @message_id = "",
-                   @timestamp = Time.epoch(0),
+                   @timestamp = Time::UNIX_EPOCH.as(Time),
                    @type = "",
                    @user_id = "",
                    @app_id = "",
@@ -176,7 +176,7 @@ module AMQP::Protocol
       flags = flags | FLAG_REPLY_TO         unless @reply_to.empty?
       flags = flags | FLAG_EXPIRATION       unless @expiration.empty?
       flags = flags | FLAG_MESSAGE_ID       unless @message_id.empty?
-      flags = flags | FLAG_TIMESTAMP        unless @timestamp.epoch == 0
+      flags = flags | FLAG_TIMESTAMP        unless @timestamp.to_unix == 0
       flags = flags | FLAG_TYPE             unless @type.empty?
       flags = flags | FLAG_USER_ID          unless @user_id.empty?
       flags = flags | FLAG_APP_ID           unless @app_id.empty?
@@ -193,7 +193,7 @@ module AMQP::Protocol
       io.write_shortstr(@reply_to)         unless @reply_to.empty?
       io.write_shortstr(@expiration)       unless @expiration.empty?
       io.write_shortstr(@message_id)       unless @message_id.empty?
-      io.write_timestamp(@timestamp)       unless @timestamp.epoch == 0
+      io.write_timestamp(@timestamp)       unless @timestamp.to_unix == 0
       io.write_shortstr(@type)             unless @type.empty?
       io.write_shortstr(@user_id)          unless @user_id.empty?
       io.write_shortstr(@app_id)           unless @app_id.empty?
@@ -616,7 +616,7 @@ module AMQP::Protocol
      end
 
      def write_timestamp(v : Time)
-       write(v.epoch.to_i64)
+       write(v.to_unix.to_i64)
      end
 
      protected def write_field(field)
@@ -705,7 +705,7 @@ module AMQP::Protocol
     def read_timestamp
       tv_sec = read_int64
       return nil unless tv_sec
-      Time.epoch tv_sec
+      Time.unix tv_sec
     end
 
     def flush
